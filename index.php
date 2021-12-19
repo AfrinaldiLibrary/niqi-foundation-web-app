509792d5bab98f31bc3aa34944dc235af10ca778
AfrinaldiLibrary <afrinaldi190401@gmail.com>
2021-12-19 21:00:23 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,311 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="owl_carousel/owl.carousel.css">
+    <link rel="stylesheet" href="owl_carousel/owl.theme.default.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Niqi Foundation</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#hero">Niqi Foundation</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ms-auto">
+                        <a class="nav-link active" aria-current="page" href="#hero">Home</a>
+                        <a class="nav-link" href="#profile">Profile</a>
+                        <a class="nav-link" href="#program">Program</a>
+                        <a class="nav-link" href="#galeri">Galeri</a>
+                        <a class="nav-link" href="#contact">Contact</a>
+                        <a class="nav-link login px-4" href="#">Login</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <section id="hero">
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container d-flex">
+                <h1>Yayasan Nur Iqsan Ibrahim</h1>
+                <p>Panti Yatim & Dhu’afa</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="profile">
+        <div class="container">
+            <div id="profil">
+                <div class="content">
+                    <h1>Profil</h1>
+                    <hr>
+                    <p class="mt-4">“ Yayasan Nur Iqsan Ibrahim merupakan yayasan yang bertujuan untuk membantu
+                        anak-anak
+                        khususnya Yatim dan Dhu’afa dalam bidang pendidikan dan kesehatan “</p>
+                </div>
+            </div>
+            <div id="visimisi">
+                <div class="display-left">
+                    <div class="visi">
+                        <div class="content">
+                            <h1>VISI</h1>
+                            <hr>
+                            <p>Menjadi lembaga sosial yang berkontribusi dalam bidang pendidikan dan kesehatan bagi
+                                anak-anak bangsa khususnya Yatim dan Dhu’afa </p>
+                        </div>
+                    </div>
+                    <div class="misi">
+                        <div class="content">
+                            <h1>MISI</h1>
+                            <hr>
+                            <ul>
+                                <li>Memberikan Kesehatan dan Pendidikan gratis bagi Yatim dan Dhu’afa</li>
+                                <li>Menyelenggarakan seminar-seminar pendidikan, kesehatan, ekonomi, kemandirian bagi
+                                    masyarakat luas, khususnya Yatim dan Dhu’afa</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="display-right">
+                    <img src="assets/img/foto1.png" alt="foto">
+                </div>
+            </div>
+
+            <div id="legalitas">
+                <div class="container wrapper">
+                    <div class="row">
+                        <h1 class="text-center">Legalitas Yayasan</h1>
+                        <hr>
+                    </div>
+                    <div class="row g-4 mx-auto owl-carousel owl-theme">
+                        <div class="col col-item mx-auto">
+                            <div class="item-img">
+                                <img src="assets/img/logo1.png" alt="logo-1" class="mx-auto img-1">
+                            </div>
+                        </div>
+
+                        <div class="col col-item mx-auto">
+                            <div class="item-img">
+                                <img src="assets/img/logo2.png" alt="logo-2" class="mx-auto img-2">
+                            </div>
+                        </div>
+
+                        <div class="col col-item mx-auto">
+                            <div class="item-img">
+                                <img src="assets/img/logo3.png" alt="logo-3" class="mx-auto img-3">
+                            </div>
+                        </div>
+
+                        <div class="col col-item mx-auto">
+                            <div class="item-img">
+                                <img src="assets/img/logo4.png" alt="logo-4" class="mx-auto img-4">
+                            </div>
+                        </div>
+
+                        <div class="col col-item mx-auto">
+                            <div class="item-img">
+                                <img src="assets/img/logo1.png" alt="logo-5" class="mx-auto img-5">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="program">
+        <div class="container">
+            <div class="title">
+                <h1>Program Yayasan</h1>
+                <hr>
+            </div>
+            <div class="container-card d-flex">
+                <div class="card d-flex">
+                    <div class="circle mx-auto">
+                        <img src="assets/img/hand.svg" alt="vector" class="d-flex mx-auto">
+                    </div>
+                    <div class="title">
+                        <h1>Program Sosial</h1>
+                    </div>
+                    <div class="description">
+                        <ul>
+                            <li>Santunan</li>
+                            <li>Bhakti Sosial</li>
+                            <li>Pembinaan Anak Jalanan</li>
+                            <li>Perpustakaan</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="card d-flex">
+                    <div class="circle mx-auto">
+                        <img src="assets/img/school.svg" alt="vector" class="d-flex mx-auto">
+                    </div>
+                    <div class="title">
+                        <h1>Program Pendidikan</h1>
+                    </div>
+                    <div class="description">
+                        <ul>
+                            <li>PAUD, TPA, MI, SDIT</li>
+                            <li>SMP, MTS, SMK, SMA, MAN</li>
+                            <li>Perguruan Tinggi</li>
+                            <li>Kepesantrenan dan Asrama</li>
+                            <li>Pusat Kegiatan dan Belajar</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="card d-flex">
+                    <div class="circle mx-auto">
+                        <img src="assets/img/horn.svg" alt="vector" class="d-flex mx-auto">
+                    </div>
+                    <div class="title">
+                        <h1>Program Dakwah</h1>
+                    </div>
+                    <div class="description">
+                        <ul>
+                            <li>Majelis Ta’lim</li>
+                            <li>Tahfidz Qur’an</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="card d-flex">
+                    <div class="circle mx-auto">
+                        <img src="assets/img/coins.svg" alt="vector" class="d-flex mx-auto">
+                    </div>
+                    <div class="title">
+                        <h1>Program Ekonomi</h1>
+                    </div>
+                    <div class="description">
+                        <ul>
+                            <li>Koperasi</li>
+                            <li>UKM & Stockis HMI/Halal Mart</li>
+                            <li>Pertanian dan Peternakan</li>
+                            <li>Klinik Kesehatan</li>
+                            <li>Percetakan</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="galeri">
+        <div class="container">
+            <div class="title">
+                <h1>Galeri</h1>
+                <hr>
+            </div>
+            <div class="container-image d-flex justify-content-between">
+                <img src="assets/img/galeri-1.jpg" alt="galeri-1">
+                <img src="assets/img/galeri-2.jpg" alt="galeri-2">
+                <img src="assets/img/galeri-3.jpg" alt="galeri-3">
+                <img src="assets/img/galeri-4.jpg" alt="galeri-4">
+                <img src="assets/img/galeri-5.jpg" alt="galeri-5">
+                <img src="assets/img/galeri-6.jpg" alt="galeri-6">
+            </div>
+            <div class="title">
+                <h1>Rekening Donasi</h1>
+                <hr>
+                <p class="mt-4">A/n Yayasan Nur Iqsan Ibrahim</p>
+            </div>
+            <div class="container-rekening">
+                <div class="item px-4">
+                    <p class="mt-3 pt-1">788 036 050 1</p>
+                    <img src="assets/img/mandiri-syariah.jpg" alt="mandiri-syariah" height="45" width="95">
+                </div>
+
+                <div class="item px-4">
+                    <p class="mt-3 pt-1">574 089 891 9</p>
+                    <img src="assets/img/bca.png" alt="bca" height="38" width="98">
+                </div>
+
+                <div class="item px-4">
+                    <p class="mt-3 pt-1">743 203 073 0</p>
+                    <img src="assets/img/btn-syariah.png" alt="btn-syariah" height="28" width="95">
+                </div>
+
+                <div class="item px-4">
+                    <p class="mt-3 pt-1">722 901 016 168 530</p>
+                    <img src="assets/img/bri.png" alt="bri" height="33" width="95">
+                </div>
+
+                <div class="item px-4">
+                    <p class="mt-3 pt-1">309 001 634 8</p>
+                    <img src="assets/img/muamala.png" alt="muamala" height="36" width="98">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="contact">
+        <div class="container">
+            <div class="title">
+                <h1>Contact</h1>
+                <hr>
+            </div>
+            <div class="container-contact d-flex justify-content-between">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8165.318749900488!2d107.00736664246594!3d-6.206341264815533!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69897d5c0f574d%3A0x1a69933329fb6b3b!2sNIQI%20FONDATION!5e0!3m2!1sid!2ssg!4v1639910168906!5m2!1sid!2ssg"
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" class="map"></iframe>
+                <div class="con-item d-flex justify-content-center align-items-center ms-4">
+                    <div class="item d-flex align-items-center">
+                        <img src="assets/img/ig.svg" alt="instagram">
+                        <p>@niqifoundation</p>
+                    </div>
+                    
+                    <div class="item d-flex align-items-center">
+                        <img src="assets/img/mail.svg" alt="email">
+                        <p>niqifoundation@gmail.com</p>
+                    </div>
+
+                    <div class="item d-flex align-items-center">
+                        <img src="assets/img/yt.svg" alt="youtube">
+                        <p>Niqi Foundation</p>
+                    </div>
+
+                    <div class="item d-flex align-items-center">
+                        <img src="assets/img/wa.svg" alt="whatsapp">
+                        <p>0857 7007 9799</p>
+                    </div>
+                </div>
+                </div>
+        </div>
+    </section>
+
+    <footer>
+        <p>Copyright 2021. All Rights Reserved Niqi Foundation</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+    <!-- jquery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+        integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <!-- carousel plugin -->
+    <script src="owl_carousel/owl.carousel.js"></script>
+    <!-- javascript -->
+    <script src="assets/js/script.js"></script>
+
+</body>
+
+</html>
